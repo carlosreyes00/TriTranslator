d0fb874f6e9001346d1c50bd84f5e50d38001ae8
Carlos Reyes <crrmagadan@icloud.com>
2025-07-19 18:05:45 -0400

    Initial Commit

---
+++ TriTranslator/TriTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriTranslatorApp.swift
+//  TriTranslator
+//
+//  Created by Carlos Reyes on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct TriTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
