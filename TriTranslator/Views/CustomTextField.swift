c67845b58e6974d0772ec7f26d978f413320fc02
Carlos Reyes <82222006+carlosreyes00@users.noreply.github.com>
2026-01-11 20:17:40 -0500

    Add .gitignore

---
+++ TriTranslator/Views/CustomTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTextField.swift
+//  TriTranslator
+//
+//  Created by Carlos Reyes on 1/11/26.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTextField()
+}
