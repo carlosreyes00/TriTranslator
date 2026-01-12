3d526a82a78f8f4404a153dcbc44b2fbf0457a89
Carlos Reyes <82222006+carlosreyes00@users.noreply.github.com>
2026-01-11 20:29:21 -0500

    add custom text field

--- TriTranslator/Views/CustomTextField.swift
+++ TriTranslator/Views/CustomTextField.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct CustomTextField: View {
+    
+    @FocusState private var isFocused: Bool
+    @Binding var text: String
+    let placeholder: String
+    let cornerRadius: CGFloat = 12
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TextField(placeholder, text: $text, axis: .vertical)
+            .focused($isFocused)
+            .lineLimit(3...6)
+            .padding(5)
+            .background(content: {
+                RoundedRectangle(cornerRadius: cornerRadius).foregroundStyle(Color(.secondarySystemBackground))
+            })
+            .overlay(
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .stroke(isFocused ? Color.accentColor : Color.black.opacity(0.1), lineWidth: 1)
+            )
+            .font(.callout)
     }
 }
 
 #Preview {
-    CustomTextField()
+    CustomTextField(text: .constant("source text"), placeholder: "Placeholder here")
 }
