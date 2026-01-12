fb9c9f8fe458e1bd8fc3268919218812bfbe2479
Carlos Reyes <82222006+carlosreyes00@users.noreply.github.com>
2026-01-11 20:39:18 -0500

    refactor LoginPage and disable interactive dismiss on it

--- TriTranslator/Pages/LoginPage.swift
+++ TriTranslator/Pages/LoginPage.swift
@@ -16,57 +16,60 @@ struct LoginPage: View {
     @State private var wrongPasswordFormat: Bool = false
     
     var body: some View {
-        Section {
-            TextField("Email", text: $email)
-                .textContentType(.emailAddress)
-            VStack (alignment: .leading) {
-                TextField("Password", text: $password)
-                    .textContentType(.password)
-                    .onChange(of: password) { _, _ in
-                        if password.count > 0 && password.count < 6 {
-                            wrongPasswordFormat = true
-                        } else {
-                            wrongPasswordFormat = false
+        VStack {
+            Section {
+                TextField("Email", text: $email)
+                    .textContentType(.emailAddress)
+                VStack (alignment: .leading) {
+                    TextField("Password", text: $password)
+                        .textContentType(.password)
+                        .onChange(of: password) { _, _ in
+                            if password.count > 0 && password.count < 6 {
+                                wrongPasswordFormat = true
+                            } else {
+                                wrongPasswordFormat = false
+                            }
+                        }
+                    
+                    if wrongPasswordFormat {
+                        Text("Your password should be at least 6 characters long")
+                            .foregroundStyle(.red)
+                            .font(.footnote)
+                    }
+                }
+            }
+            .padding(.horizontal)
+            
+            HStack {
+                Button("Sign In") {
+                    Task {
+                        do {
+                            print("signing in")
+                            try await authViewModel
+                                .signIn(email: email, password: password)
+                            print("signed in")
+                        } catch {
+                            print(error.localizedDescription)
                         }
                     }
+                }
                 
-                if wrongPasswordFormat {
-                    Text("your password should be at least 6 characters long")
-                        .foregroundStyle(.red)
-                        .font(.footnote)
-                }
-            }
-        }
-        .padding(.horizontal)
-        
-        HStack {
-            Button("Sign In") {
-                Task {
-                    do {
-                        print("signing in")
-                        try await authViewModel
-                            .signIn(email: email, password: password)
-                        print("signed in")
-                    } catch {
-                        print(error.localizedDescription)
+                Button("Sign Up") {
+                    Task {
+                        do {
+                            print("signing up")
+                            try await authViewModel
+                                .signUp(email: email, password: password)
+                            print("signed up")
+                        } catch {
+                            print(error.localizedDescription)
+                        }
                     }
                 }
             }
-            
-            Button("Sign Up") {
-                Task {
-                    do {
-                        print("signing up")
-                        try await authViewModel
-                            .signUp(email: email, password: password)
-                        print("signed up")
-                    } catch {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
+            .buttonStyle(.borderedProminent)
         }
-        .buttonStyle(.borderedProminent)
+        .interactiveDismissDisabled()
     }
 }
 
